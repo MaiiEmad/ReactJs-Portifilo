ca764703529563fdf7d2be6ec5f90af0d0fd24c7
Maii Emad EL-Deen <eng.maii.emad@gmail.com>
2022-04-16 15:01:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import HeroSection from './HeroSection';
+import BioSection  from './BioSection';
+import SkillsSection  from './SkillsSection';
+import PortolioSection  from './PortolioSection';
+import FooterSection from './FooterSection';
+
+
+function App() {
+  return (
+    <>
+    <HeroSection/>
+    <BioSection/>
+    <SkillsSection/>
+    <PortolioSection/>
+    <FooterSection/>
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/HeroSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function HeroSection() {
+    return (
+        <>
+      <div className="main">
+         <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2> kaite Reed</h2>
+                        <h1>Web Developer & Designer</h1>
+                        <button type="button" class="col-md-6 text-center btn btn-dark">CONTACT ME</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+
+
+
+export default HeroSection;
+
+
+
+
+
+
+
+
+
+
